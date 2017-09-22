1998d5ebedbb99aad607b46e8d06468635f38898
dubeejw <jwdubee@us.ibm.com>
2017-09-22 18:50:20 -0400

    Prevent unauthorized access to actions via sequences

--- core/controller/src/main/scala/whisk/core/entitlement/Entitlement.scala
+++ core/controller/src/main/scala/whisk/core/entitlement/Entitlement.scala
@@ -325,6 +325,10 @@ trait ReferencedEntities {
         e.components.map { c =>
           Resource(c.path, Collection(Collection.ACTIONS), Some(c.name.asString))
         }.toSet
+      case e: SequenceExec2 =>
+        e.components.map { c =>
+          Resource(c.path, Collection(Collection.ACTIONS), Some(c.name.asString))
+        }.toSet
       case _ => Set()
     }
   }
